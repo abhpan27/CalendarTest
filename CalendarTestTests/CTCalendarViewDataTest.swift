c557032b19b702643e141030ae38b5abf6631a33
Abhishek Kumar Pandey <abhishek.pandey@webyog.com>
2018-01-16 15:47:13 +0530

    Event availability dot color Unit test is added.

--- CalendarTestTests/CTCalendarViewDataTest.swift
+++ CalendarTestTests/CTCalendarViewDataTest.swift
@@ -14,18 +14,21 @@ class CTCalendarViewDataTest: XCTestCase {
 	var minDateToShowInCal:Date!
 	var maxDateToShowInCal:Date!
 	var calUIDataHelper:CTCalViewDataHelper!
+	var dbQueryHelper:CTDBQueryDataHelper!
 
     override func setUp() {
 		let minMaxDate = CTAppConstants.shared.minMaxDate
 		self.minDateToShowInCal = minMaxDate.minDate
 		self.maxDateToShowInCal = minMaxDate.maxDate
 		self.calUIDataHelper = CTCalViewDataHelper()
+		self.dbQueryHelper = CTDBQueryDataHelper()
         super.setUp()
     }
     
     override func tearDown() {
      	self.maxDateToShowInCal = nil
 		self.maxDateToShowInCal = nil
+		self.dbQueryHelper = nil
         super.tearDown()
     }
 
@@ -84,6 +87,75 @@ class CTCalendarViewDataTest: XCTestCase {
 		//there should be same no of months in collection view and overlay month tableview
 		let noMonthsBetweenMinAndMaxDates = self.maxDateToShowInCal!.months(from: self.minDateToShowInCal!)
 		XCTAssert((noMonthsBetweenMinAndMaxDates + 1) == calTableMonthDataIndex, "Mismatch bewtween no of months to show in collection view and data generated for overlay table view")
+	}
+
+	func testEventAvailabilityForCalCollectionView() {
+		calUIDataHelper.loadBaicUIdata()
+
+		let expectation = self.expectation(description: "Async DB fetch for event availability")
+
+		let completeRangeContentRequest = CTDBQueryContentRequest(fromDate: CTAppConstants.shared.minMaxDate.minDate, endDate: CTAppConstants.shared.minMaxDate.maxDate, type: .calViewdata)
+		dbQueryHelper.dictionaryOfEventAvalabilty(forContentRequest: completeRangeContentRequest) {
+			[weak self]
+			(expectedEventAvailabiltyDict, error)
+			in
+			guard let blockSelf = self
+				else{
+					return
+			}
+			
+			//event availabilty dict should not be nil and error should be nil
+			XCTAssert(expectedEventAvailabiltyDict != nil, "Event availability was nil")
+			XCTAssert(error == nil, "Got error while quering DB for event availability")
+
+			//now load using UI data helper
+			blockSelf.calUIDataHelper.loadEventAvailabiltyForShowingDots {
+				[weak self]
+				in
+				guard let blockSelf = self
+					else {
+						return
+				}
+
+				blockSelf.validateEventAvailability(withExpectedAvailability: expectedEventAvailabiltyDict!)
+				expectation.fulfill()
+			}
+		}
+
+		waitForExpectations(timeout: 5.0) { (error) in
+			if let error = error {
+				XCTFail("waiting with error: \(error.localizedDescription)")
+			}
+		}
+	}
+
+	private func validateEventAvailability(withExpectedAvailability:[Date:Int]) {
+		for row in 0 ... self.calUIDataHelper.calCollectionViewUIData.count - 1 {
+			for coloum in 0 ... 6 {
+				let currCellData = self.calUIDataHelper.calCollectionViewUIData[row][coloum]
+				guard !currCellData.isBlankDay
+					else {
+						continue
+				}
+
+				let dateForCurrCell = Date(timeIntervalSince1970: currCellData.dateEpoch)
+				if let expectedAvailabilty = withExpectedAvailability[dateForCurrCell]  {
+					if expectedAvailabilty == 1 {
+						XCTAssert(currCellData.eventAvailabilityColor == CalendarViewCellEventAvailablityColor.lightGrey.rawValue, "For only one event on day event availability dot color should be light grey")
+					}
+
+					if expectedAvailabilty == 2 {
+						XCTAssert(currCellData.eventAvailabilityColor == CalendarViewCellEventAvailablityColor.mediumGrey.rawValue, "For two events on day event availability dot color should be medium grey")
+					}
+
+					if expectedAvailabilty >= 3 {
+						XCTAssert(currCellData.eventAvailabilityColor == CalendarViewCellEventAvailablityColor.darkGrey.rawValue, "For three events on day event availability dot color should be dark grey")
+					}
+				}else {
+						XCTAssert(currCellData.eventAvailabilityColor == CalendarViewCellEventAvailablityColor.clear.rawValue, "For no events on day event availability dot color should be clear color")
+				}
+			}
+		}
 	}
 
 }
